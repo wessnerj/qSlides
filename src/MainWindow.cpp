d48e5da585b16acdd22a681c799ecfba3f69179a
Joseph Wessner <Joseph.Wessner@googlemail.com>
2013-03-08 10:19:31 +0100

    Adding poppler and C++11

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -9,6 +9,7 @@
 #include "ui_mainWindow.h"
 
 #include <iostream>
+#include "poppler-qt4.h"
 
 #include <QFileDialog>
 
@@ -25,4 +26,22 @@ void MainWindow::on_actionOpen_File_triggered() {
 	QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"), "",
 			tr("PDF Documents (*.pdf)"));
 	std::cout << "Chosen file: " << fileName.toStdString() << std::endl;
+
+	Poppler::Document *doc = Poppler::Document::load(fileName);
+
+	if (nullptr == doc) {
+		std::cout << "Document not loaded" << std::endl;
+	} else {
+		std::cout << "Document loaded" << std::endl;
+
+		physicalDpiX();
+
+		Poppler::Page *page = doc->page(0);
+		QImage renderedPDF = page->renderToImage(physicalDpiX(),
+				physicalDpiX());
+		delete page;
+		page = nullptr;
+	}
+
+	showFullScreen();
 }
